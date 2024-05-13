fd2da3c84b89d1c93300b17c404102915c5d1061
Cristian <contato@camargodev.com.br>
2024-05-12 22:18:20 -0300

    feat: add product fetch

--- app.vue
+++ app.vue
@@ -6,5 +6,15 @@
       <SectionProducts />
       <SectionAbout />
       <SectionContact />
+      <SectionFooter />
     </main>
 </template>
+
+<script setup>
+useHead({
+    title: 'DR COSMÉTICOS E ESSÊNCIAS',
+    meta: [
+      { name: 'description', content: 'Descubra nossa linha Via Aroma e encontre a fragrância perfeita para seu espaço. Contate um consultor hoje e transforme ambientes com aromas que inspiram.' }
+    ],
+})
+</script>
---
+++ components/sections/SectionFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="w-full bg-pink py-20">
+        <Container class="grid grid-cols-2 items-center">
+            <div class="text-ligth">
+                <span>©2022 DR Cosméticos e Essencias</span>
+                <p>Todos os direitos reservados.</p>
+            </div>
+
+            <div class="flex justify-end">
+                <nuxt-img src="/logo.png" alt="Logo" width="100" height="100"/>
+            </div>
+        </Container>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
